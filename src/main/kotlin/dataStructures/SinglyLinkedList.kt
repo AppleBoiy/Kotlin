ed8631f36d36cf422fa356bb31542e380e450ddc
Chaipat J. <work.chaipat@gmail.com>
2024-04-04 07:43:07 +0700

    add reversed, sorted, insertAtIndex, etc., to LinkedList

--- src/main/kotlin/dataStructures/SinglyLinkedList.kt
+++ src/main/kotlin/dataStructures/SinglyLinkedList.kt
@@ -1,7 +1,7 @@
 package dataStructures
 
 interface Node<T> {
-    val value: T
+    var value: T
     var next: Node<T>?
 }
 
@@ -14,6 +14,17 @@ interface Node<T> {
 class SinglyLinkedList {
     private var head: Node<Int>? = null
 
+    val length: Int
+        get() {
+            var current = head
+            var count = 0
+            while (current != null) {
+                count++
+                current = current.next
+            }
+            return count
+        }
+
     /**
      * Insert a new node with the given value at the end of the linked list
      * @param value The value to be inserted
@@ -25,7 +36,7 @@ class SinglyLinkedList {
      */
     fun insert(value: Int) {
         val newNode = object : Node<Int> {
-            override val value = value
+            override var value = value
             override var next: Node<Int>? = null
         }
 
@@ -38,6 +49,55 @@ class SinglyLinkedList {
             }
             current?.next = newNode
         }
+    }
+
+    /**
+     * Insert a new node with the given value at the given index in the linked list
+     * @param value The value to be inserted
+     * @param index The index at which the value should be inserted
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     * @sample dataStructures.SinglyLinkedListTest.insertAtIndex
+     */
+    fun insert(value: Int, index: Int) {
+        if (index < 0 || index > length) {
+            throw IndexOutOfBoundsException("Index: $index, Length: $length")
+        }
+
+        val newNode = object : Node<Int> {
+            override var value = value
+            override var next: Node<Int>? = null
+        }
+
+        if (index == 0) {
+            newNode.next = head
+            head = newNode
+        } else {
+            var current = head
+            var i = 0
+            while (i < index - 1) {
+                current = current?.next
+                i++
+            }
+            newNode.next = current?.next
+            current?.next = newNode
+        }
+    }
+
+    /**
+     * Delete first node from the linked list and return its value
+     *
+     * @return the value of the deleted node
+     * @throws NoSuchElementException if the linked list is empty
+     * @sample dataStructures.SinglyLinkedListTest.pop
+     */
+    fun pop(): Int {
+        if (head == null) {
+            throw NoSuchElementException("Linked list is empty")
+        }
+
+        val value = head!!.value
+        head = head!!.next
+        return value
     }
 
     /**
@@ -72,24 +132,87 @@ class SinglyLinkedList {
     /**
      * Search for the node with the given value in the linked list
      * @param value The value to be searched
-     * @return true if the value is found, false otherwise
+     * @return index of the value returned if found, -1 otherwise
      *
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      *
      * @sample dataStructures.SinglyLinkedListTest.search
      */
-    fun search(value: Int): Boolean {
+    fun search(value: Int): Int {
         var current = head
+        var index = 0
         while (current != null) {
             if (current.value == value) {
-                return true
+                return index
+            }
+            current = current.next
+            index++
+        }
+        return -1
+    }
+
+    /**
+     * Reverse the linked list
+     *
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     *
+     * @sample dataStructures.SinglyLinkedListTest.reverse
+     */
+    fun reversed() {
+        var prev: Node<Int>? = null
+        var current = head
+        var next: Node<Int>?
+        while (current != null) {
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+        head = prev
+    }
+
+    /**
+     * Sort the linked list
+     *
+     * Time Complexity: O(n^2)
+     * Space Complexity: O(1)
+     *
+     * @sample dataStructures.SinglyLinkedListTest.sort
+     */
+    fun sorted() {
+        var current = head
+        var index: Node<Int>?
+        var temp: Int
+        while (current != null) {
+            index = current.next
+            while (index != null) {
+                if (current.value > index.value) {
+                    temp = current.value
+                    current.value = index.value
+                    index.value = temp
+                }
+                index = index.next
             }
             current = current.next
         }
-        return false
     }
 
+    /**
+     * Copy the linked list
+     * @return a new linked list with the same values
+     * @sample dataStructures.SinglyLinkedListTest.copy
+     */
+    fun copy(): SinglyLinkedList {
+        val newList = SinglyLinkedList()
+        var current = head
+        while (current != null) {
+            newList.insert(current.value)
+            current = current.next
+        }
+        return newList
+    }
 
     /**
      * @return string representation of the linked list
--- src/test/kotlin/dataStructures/SinglyLinkedListTest.kt
+++ src/test/kotlin/dataStructures/SinglyLinkedListTest.kt
@@ -30,6 +30,18 @@ class SinglyLinkedListTest {
     }
 
     @Test
+    fun insertAtIndex() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        singlyLinkedList.insert(4, 1)
+
+        assertEquals("1 4 2 3", singlyLinkedList.toString())
+    }
+
+    @Test
     fun delete() {
         val singlyLinkedList = SinglyLinkedList()
         singlyLinkedList.insert(1)
@@ -48,6 +60,75 @@ class SinglyLinkedListTest {
         singlyLinkedList.insert(2)
         singlyLinkedList.insert(3)
 
-        assertTrue(singlyLinkedList.search(2))
+        assertEquals(1, singlyLinkedList.search(2))
+    }
+
+    @Test
+    fun searchNotFound() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        assertEquals(-1, singlyLinkedList.search(4))
+    }
+
+    @Test
+    fun reverse() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        singlyLinkedList.reversed()
+
+        assertEquals("3 2 1", singlyLinkedList.toString())
+    }
+
+    @Test
+    fun sort() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(3)
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+
+        singlyLinkedList.sorted()
+
+        assertEquals("1 2 3", singlyLinkedList.toString())
+    }
+
+    @Test
+    fun length() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        assertEquals(3, singlyLinkedList.length)
+    }
+
+    @Test
+    fun copy() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        val copiedList = singlyLinkedList.copy()
+
+        assertEquals("1 2 3", copiedList.toString())
+    }
+
+    @Test
+    fun pop() {
+        val singlyLinkedList = SinglyLinkedList()
+        singlyLinkedList.insert(1)
+        singlyLinkedList.insert(2)
+        singlyLinkedList.insert(3)
+
+        val popped = singlyLinkedList.pop()
+
+        assertEquals("2 3", singlyLinkedList.toString())
+        assertEquals(1, popped)
     }
 }
